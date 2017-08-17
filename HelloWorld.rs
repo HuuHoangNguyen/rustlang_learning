c0c982d473bc0a863746e142ca547573550f28e2
HuuHoangNguyen <hoangnh.hpp@gmail.com>
2017-08-17 16:11:23 +0700

    Update 2017-08-17_16h10

--- HelloWorld.rs
+++ HelloWorld.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello RustLang! \n");
+fn main() 
+{
+    let mut a = 5;
+    let (x, y) = (10, 20);
+    
+    println!("Hello RustLang! a = {}\n", a);
+    println!("Hello RustLang! x & y = {0} & {1}\n", x, y);
+    a = 10;
+    println!("Hello RustLang! a = {}\n", a);
+ 
+    let b : i32 = 8;
+    println!("Hello RustLang! b = {}\n", b);
+
+
+    let m : i32 = 17;
+    {
+        let n : i32 = 10;
+         println!("The value of m & n is {} & {}", m, n);
+    }
+    
+    //intln!("The value of m & n is {} & {}", m, n);
+
+
+    let c : i32 = 8;
+    {
+        println!("{}", c); // Prints "8"
+        
+        let c = 12;          
+        println!("{}", c); // Prints "12"
+    }
+	
+    println!("{}", c); // Prints "8"
+    let c = 42;
+
+
+    println!("{}", c); // Prints "42"
+
 }
---
+++ P_01.rs
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+fn main()
+{
+    let mut x : i32  = 0;
+    x = 7;
+    
+    let x = x; // x is immutable now and is bound to 7
+   
+    let y = 4;
+    let y = "I can also be bound to text!"; // y is of a different type now
+}
